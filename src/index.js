422e2fd266c153d97fa416710eea8bf638da4c97
haider_ali <innoxentmughalforever@gmail.com>
2024-02-07 01:52:25 +0500

    api updated

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ function component() {
         const format = item.format()
         ancher.setAttribute("id", item.name)
         ancher.textContent = "[project url]/api/" + format
-        ancher.setAttribute("href", `http://localhost:8080/api/${format}`)
+        ancher.setAttribute("href", `https://time-stamp-backend.vercel.app/api/${format}`)
 
         li.appendChild(ancher)
         unOrderList.appendChild(li)
